4605d7a56eabc7e5f9b38032a6fc49b26f5ee8c2
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-09 00:43:14 +0300

    feat: create `auth` routes and validator

---
+++ routes/auth.routes.js
@@ -0,0 +1,115 @@
+import express from "express";
+
+import authController from "../controllers/auth.controller.js";
+import authValidator from "../validators/auth.validator.js";
+
+import rateLimiters from "../middleware/rate-limit.middleware.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Sign up
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               name: "Name"
+ *               surname: "Surname"
+ *               email: "namesurname@domain.com"
+ *               password: "123456"
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+router.post("/signup", rateLimiters.auth, authValidator.signup, authController.signup);
+
+/**
+ * @swagger
+ * /api/auth/signin:
+ *   post:
+ *     summary: Sign in
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: "namesurname@domain.com"
+ *               password: "123456"
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.post("/signin", rateLimiters.auth, authValidator.signin, authController.signin);
+
+/**
+ * @swagger
+ * /api/auth/refresh:
+ *   put:
+ *     summary: Refresh tokens
+ *     description: Refresh the access token using the refresh token.
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *             example:
+ *               refreshToken: ""
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.put("/refresh", rateLimiters.auth, authValidator.refresh, authController.refresh);
+
+/**
+ * @swagger
+ * /api/auth/signout:
+ *   delete:
+ *     summary: Sign out
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *             example:
+ *               refreshToken: ""
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.delete("/signout", rateLimiters.auth, authValidator.signout, authController.signout);
+
+export default router;
