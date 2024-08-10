f3c879d1287b4dad075fa244905a7e07edec1376
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-10 14:54:22 +0300

    feat: add `authors` routes to `index` routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,11 +3,12 @@ import express from "express";
 import indexController from "../controllers/index.controller.js";
 
 import authRotes from "./auth.routes.js";
+import authorsRoutes from "./authors.routes.js";
 import swaggerRoutes from "./swagger.routes.js";
 
 const router = express.Router();
 
 router.get("/", indexController.welcomePage);
-router.use("/auth", authRotes).use("/swagger", swaggerRoutes);
+router.use("/auth", authRotes).use("/authors", authorsRoutes).use("/swagger", swaggerRoutes);
 
 export default router;
