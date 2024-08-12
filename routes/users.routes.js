f0d4678af83d0cc5470339cbf9e80e105f66b5b0
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-12 21:42:51 +0300

    feat: add routes and validators to `users` for updating `user profile`

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,7 @@ import express from "express";
 import usersController from "../controllers/users.controller.js";
 import usersValidator from "../validators/users.validator.js";
 
+import authCheck from "../middleware/auth-check.middleware.js";
 import rateLimiters from "../middleware/rate-limit.middleware.js";
 
 const router = express.Router();
@@ -24,5 +25,148 @@ const router = express.Router();
  *         description: OK
  */
 router.get("/:userID", rateLimiters.common, usersValidator.getUser, usersController.getUser);
+
+/**
+ * @swagger
+ * /api/users/{userID}:
+ *   put:
+ *     summary: Update User Information by ID
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                 type: string
+ *             example:
+ *               name: "John"
+ *               surname: "Doe"
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.put(
+  "/:userID",
+  rateLimiters.database,
+  authCheck.isSignIn,
+  usersValidator.updateUser,
+  usersController.updateUser
+);
+
+/**
+ * @swagger
+ * /api/users/{userID}/email:
+ *   put:
+ *     summary: Update User Email by ID
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               oldEmail:
+ *                 type: string
+ *               newEmail:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               oldEmail: "namesurname@domain.com"
+ *               newEmail: "name@domain.com"
+ *               password: "123456"
+ */
+router.put(
+  "/:userID/email",
+  rateLimiters.database,
+  authCheck.isSignIn,
+  usersValidator.updateEmail,
+  usersController.updateEmail
+);
+
+/**
+ * @swagger
+ * /api/users/{userID}/password:
+ *   put:
+ *     summary: Update User Password by ID
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               oldPassword:
+ *                 type: string
+ *               newPassword:
+ *                 type: string
+ *             example:
+ *               oldPassword: "123456"
+ *               newPassword: "987654"
+ */
+router.put(
+  "/:userID/password",
+  rateLimiters.database,
+  authCheck.isSignIn,
+  usersValidator.updatePassword,
+  usersController.updatePassword
+);
+
+/**
+ * @swagger
+ * /api/users/{userID}:
+ *   delete:
+ *     summary: Deactivate User by ID
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.delete(
+  "/:userID",
+  rateLimiters.database,
+  authCheck.isSignIn,
+  usersValidator.deactivateUser,
+  usersController.deactivateUser
+);
 
 export default router;
