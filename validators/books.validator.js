2bdbb0e8752abbe960847cd1ced5b84a47bfc3d4
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-13 14:07:23 +0300

    feat: add `update` and `delete` functionality to `books` routes and validator

--- validators/books.validator.js
+++ validators/books.validator.js
@@ -1,6 +1,16 @@
 import { body, param, query } from "express-validator";
 import { validationCheck } from "../middleware/validation-check.middleware.js";
 
+const sharedValidator = {
+  params: {
+    bookID: param("bookID")
+      .notEmpty()
+      .withMessage("Book ID is required.")
+      .isInt()
+      .withMessage("Book ID must be an integer."),
+  },
+};
+
 const booksValidator = {
   createBook: [
     body("title")
@@ -55,6 +65,19 @@ const booksValidator = {
       .withMessage("Per page must be a positive integer."),
     validationCheck,
   ],
+  updateBook: [
+    sharedValidator.params.bookID,
+    body("title").optional().isString().withMessage("Title must be a string."),
+    body("authorID").optional().isInt().withMessage("Author ID must be an integer."),
+    body("image").optional().isURL().withMessage("Image must be a valid URL."),
+    body("publisherID").optional().isInt().withMessage("Publisher ID must be an integer."),
+    body("published").optional().isISO8601().withMessage("Published date must be a valid date."),
+    body("isbn13").optional().isISBN(13).withMessage("ISBN-13 must be a valid ISBN-13 number."),
+    body("isbn10").optional().isISBN(10).withMessage("ISBN-10 must be a valid ISBN-10 number."),
+    body("status").optional().isString().withMessage("Status must be a string."),
+    validationCheck,
+  ],
+  deleteBook: [sharedValidator.params.bookID, validationCheck],
 };
 
 export default booksValidator;
