9ca56d3943f786cdcd3eccc09d9c5955e1fc02c7
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-25 18:43:06 +0300

    chore: add `JSDoc` comments for `index.config` (#24)

--- config/index.config.js
+++ config/index.config.js
@@ -47,6 +47,40 @@ const env = envSchema({
   },
 });
 
+/**
+ * @global
+ * @typedef {Object} Config
+ *
+ * @property {Object} server - Server configuration settings.
+ * @property {number|string} server.port - The port number for the server to listen on.
+ * @property {string} server.origin - The origin URL of the server.
+ * @property {string} server.node_env - The environment the server is running in (e.g., development, production).
+ *
+ * @property {Object} database - Database configuration settings (to be defined).
+ *
+ * @property {Object} cache - Cache configuration settings.
+ * @property {Object} cache.redis - Redis configuration settings.
+ * @property {string} cache.redis.host - The host address of the Redis server.
+ * @property {number|string} cache.redis.port - The port number of the Redis server.
+ *
+ * @property {Object} token - Token configuration settings.
+ * @property {Object} token.access - Access token configuration settings.
+ * @property {string} token.access.secret - Secret key used to sign the access token.
+ * @property {Object} token.access.options - Options for the access token.
+ * @property {string|number} token.access.options.expiresIn - Expiration time for the access token.
+ *
+ * @property {Object} token.refresh - Refresh token configuration settings.
+ * @property {string} token.refresh.secret - Secret key used to sign the refresh token.
+ * @property {Object} token.refresh.options - Options for the refresh token.
+ * @property {string|number} token.refresh.options.expiresIn - Expiration time for the refresh token.
+ *
+ * @property {Object} logger - Logger configuration settings.
+ * @property {Object} logger.file - File logging configuration settings.
+ * @property {string} logger.file.level - Logging level for file logs (e.g., error, debug).
+ * @property {string} logger.file.filename - File path for storing logs.
+ * @property {Object} logger.console - Console logging configuration settings.
+ * @property {string} logger.console.level - Logging level for console logs (e.g., warn, debug).
+ */
 const config = {
   server: {
     port: env.PORT,
@@ -82,7 +116,7 @@ const config = {
     console: {
       level: env.NODE_ENV === "production" ? "warn" : "debug",
     },
-  }
+  },
 };
 
 export default config;
