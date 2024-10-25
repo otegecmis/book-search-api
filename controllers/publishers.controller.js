1c400f1bf351a64a33db1cf9bdb194f79b90fc8f
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-10-25 16:56:45 +0300

    chore: remove `JSDoc` comments from `authors` and `publishers` controller

--- controllers/publishers.controller.js
+++ controllers/publishers.controller.js
@@ -2,17 +2,11 @@ import publishersService from "../services/publishers.service.js";
 
 /**
  * PublishersController class.
- * @class
- * @classdesc Controller class for managing publishers.
+ * @ignore
  */
 class PublishersController {
   /**
    * Creates a new publisher.
-   * @param {object} req - The request object.
-   * @param {object} res - The response object.
-   * @param {Function} next - The next middleware function for error handling.
-   * @returns {Promise<object>} - A promise that resolves to the created publisher object.
-   * @throws {Error} - If an error occurs while creating the publisher, such as validation errors or database issues.
    */
   async createPublisher(req, res, next) {
     try {
@@ -30,11 +24,6 @@ class PublishersController {
 
   /**
    * Retrieves a paginated list of publishers.
-   * @param {object} req - The request object.
-   * @param {object} res - The response object.
-   * @param {Function} next - The next middleware function.
-   * @returns {Promise<object>} - A promise that resolves to an object containing the list of publishers and pagination information.
-   * @throws {Error} - If an error occurs while retrieving the publishers.
    */
   async getPublishers(req, res, next) {
     try {
@@ -49,11 +38,6 @@ class PublishersController {
 
   /**
    * Retrieves a publisher by ID.
-   * @param {object} req - The request object.
-   * @param {object} res - The response object.
-   * @param {Function} next - The next middleware function.
-   * @returns {Promise<object>} - A promise that resolves to the publisher object if found.
-   * @throws {Error} - If an error occurs while retrieving the publisher or if the publisher is not found.
    */
   async getPublisherByID(req, res, next) {
     try {
@@ -68,11 +52,6 @@ class PublishersController {
 
   /**
    * Updates a publisher by ID.
-   * @param {object} req - The request object.
-   * @param {object} res - The response object.
-   * @param {Function} next - The next middleware function.
-   * @returns {Promise<object>} - A promise that resolves to the updated publisher object.
-   * @throws {Error} - If an error occurs while updating the publisher or if the publisher is not found.
    */
   async updatePublisher(req, res, next) {
     try {
@@ -91,11 +70,6 @@ class PublishersController {
 
   /**
    * Deletes a publisher by ID.
-   * @param {object} req - The request object.
-   * @param {object} res - The response object.
-   * @param {Function} next - The next middleware function.
-   * @returns {Promise<void>} - A promise that resolves when the publisher is successfully deleted.
-   * @throws {Error} - If an error occurs while deleting the publisher or if the publisher is not found.
    */
   async deletePublisher(req, res, next) {
     try {
